51cecac23ae8cfce43f4fbd5150c1e0fbe7cbefb
ssickles <scottsickles@gmail.com>
2011-11-23 17:35:32 -0500

    - added WebSocket test app

---
+++ WebSockets/WebSocketServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.ServiceModel.WebSockets;
+
+namespace WebSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebSocketsHost<ReverseService>();
+            host.AddWebSocketsEndpoint("ws://void:9999/reverse");
+            host.Open();
+            Console.WriteLine("The reverse service is now hosted as a Web Sockets service.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebSockets/WebSocketServer/ReverseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.ServiceModel.WebSockets;
+
+namespace WebSocketServer
+{
+    class ReverseService: WebSocketsService
+    {
+        public override void OnMessage(string value)
+        {
+            string reverseString = string.Join("", value.Reverse());
+            SendMessage(reverseString);
+        }
+    }
+}
